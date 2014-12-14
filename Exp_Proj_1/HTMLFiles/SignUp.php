df8168c7f4b1f62df089bf19151e69cf3a642925
anck <myscoob@gmail.com>
2014-12-14 16:01:35 -0500

    Adding HTML FIles.

---
+++ Exp_Proj_1/HTMLFiles/SignUp.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="generator" content="CoffeeCup HTML Editor (www.coffeecup.com)">
+    <meta name="dcterms.created" content="Sun, 14 Dec 2014 17:58:40 GMT">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Index.php</title>
+    
+    <style type="text/css">
+    
+    body {
+      color:#000000;
+      background-color:#FFFFFF;
+      background-image:url('Background Image');
+      background-repeat:no-repeat;
+    }
+    a  { color:#000FFF; }
+    a:visited { color:#800080; }
+    a:hover { color:#008000; }
+    a:active { color:#FF0000; }
+    
+    </style>
+    <!--[if IE]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+</head>
+<body>
+
+	<hr>
+    <form action="welcome_get.php" method="get">
+    First Name: <input type="text" name="Fname"><br>
+	Last Name: <input type="text" name="Lname"><br>
+	Gender:</br>
+	<input type="checkbox" name="option1" value="Male"> Milk<br>
+	<input type="checkbox" name="option2" value="Female" checked> Butter<br>
+	E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+    </form>
+
+</body>
+</html>
---
+++ Exp_Proj_1/HTMLFiles/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="generator" content="CoffeeCup HTML Editor (www.coffeecup.com)">
+    <meta name="dcterms.created" content="Sun, 14 Dec 2014 17:58:40 GMT">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Index.php</title>
+    
+    <style type="text/css">
+    <!--
+    body {
+      color:#000000;
+      background-color:#FFFFFF;
+      background-image:url('Background Image');
+      background-repeat:no-repeat;
+    }
+    a  { color:#0000FF; }
+    a:visited { color:#800080; }
+    a:hover { color:#008000; }
+    a:active { color:#FF0000; }
+    -->
+    </style>
+    <!--[if IE]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+	
+	
+	ME GUSTA 
+	<hr>
+	<a href="SignUp.php">Sign Up</a>
+	</br>
+	<a href="LogIn.php">Log In</a>
+</head>
+</body>
+</html>
